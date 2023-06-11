7f8715a14842170fdb3f60f258c275730377f1fc
Jeff Nelson <jpnelson85@gmail.com>
2023-06-11 10:01:46 -0500

    finish user story1

---
+++ spec/features/machines/show_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'When a user visits a vending machine show page', type: :feature do
+  scenario 'they see the location of that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+  it 'display all snacks asssociated with machine along with price' do
+    owner = Owner.create!(name: "Sam's Snacks")
+    dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+    snack_1 = Snack.create!(name: "Cheetos", price: 2.50)
+    snack_2 = Snack.create!(name: "Doritos", price: 3.50)
+    snack_3 = Snack.create!(name: "Fritos", price: 4.50)
+    MachineSnack.create!(machine: dons, snack: snack_1)
+    MachineSnack.create!(machine: dons, snack: snack_2)
+
+    visit machine_path(dons)
+
+    within "#snacks" do
+      expect(page).to have_content(snack_1.name)
+      expect(page).to have_content(snack_2.name)
+      expect(page).to_not have_content(snack_3.name)
+      expect(page).to have_content(snack_1.price)
+      expect(page).to have_content(snack_2.price)
+      expect(page).to_not have_content(snack_3.price)
+    end
+  end
+
+  it 'display average price for all snacks in machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    machine2 = owner.machines.create(location: "Jeff's Mixed Drinks")
+    snack_1 = Snack.create(name: "Cheetos", price: 2.50)
+    snack_2 = Snack.create(name: "Doritos", price: 3.50)
+    snack_3 = Snack.create(name: "Fritos", price: 4.50)
+    MachineSnack.create(machine: dons, snack: snack_1)
+    MachineSnack.create(machine: dons, snack: snack_2)
+    MachineSnack.create(machine: machine2, snack: snack_3)
+
+    visit machine_path(dons)
+save_and_open_page
+    within "#average_price" do
+      expect(page).to have_content(dons.average_price)
+      expect(page).to_not have_content(machine2.average_price)
+    end
+  end
+end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks)}
   end
+
+  it 'can show average snack price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    machine2 = owner.machines.create(location: "Jeff's Mixed Drinks")
+    snack_1 = Snack.create(name: "Cheetos", price: 2.50)
+    snack_2 = Snack.create(name: "Doritos", price: 3.50)
+    snack_3 = Snack.create(name: "Fritos", price: 4.50)
+    MachineSnack.create(machine: dons, snack: snack_1)
+    MachineSnack.create(machine: dons, snack: snack_2)
+    MachineSnack.create(machine: machine2, snack: snack_3)
+    
+    expect(dons.average_price).to eq(3.0)
+    expect(machine2.average_price).to eq(4.50)
+  end
 end
